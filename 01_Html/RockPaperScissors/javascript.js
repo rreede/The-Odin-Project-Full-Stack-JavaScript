ca8ad075041c8c226e0da90e33c03114c0911882
Rene Reede <rene.reede@gmail.com>
2023-11-29 20:32:43 +0200

    Feature: Add RockPaperScissors Game
    - Added computer selection and player selection mechanism
    - Added game playing function

---
+++ 01_Html/RockPaperScissors/javascript.js
@@ -0,0 +1,39 @@
+function getComputerChoice() {
+
+    let moves = ["ROCK","PAPER","SCISSORS"];
+    let random = Math.floor((Math.random() * moves.length));
+    let computerChoice =  moves[random];
+    console.log(`Computer chose: ${computerChoice}`);
+    return computerChoice;
+   
+}
+
+function getPlayerChoice() {
+
+    let playerChoice = prompt("Rock, Paper or Scissors?");
+    let upperCasePlayerChoice = playerChoice.toUpperCase();
+    return upperCasePlayerChoice;
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    if(playerSelection === computerSelection) {
+        console.log("TIE! Play again");
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        console.log("You LOSE!");
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        console.log("You WIN!");
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        console.log("You WIN!");
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        console.log("You LOSE!");
+    }  else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        console.log("You LOSE!");
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        console.log("You WIN!");
+    } 
+
+}
+
+playRound(getPlayerChoice(), getComputerChoice());
---
+++ 01_Html/javascript.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let admin;
+
+let name;
+
+name = 'John';
+
+admin = name;
+
+alert(admin);
+
+let ourPlanet = 'Earth';
+
+let currentVisitor = 'Pedro';
+
+
+let a = 2;
+
+switch (a) {
+
+    case 1:
+        console.log('Hello');
+        break;
+    case 2:
+        console.log('yes');
+        break;
+    default:
+        console.log('Default');
+}
+
+
+console.log(`Hello, I am Jason and I am ${42 - a} years old`);
