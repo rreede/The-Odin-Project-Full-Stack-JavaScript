bb2079fdfa8013906cfd1422cfcdfd07e45097d7
Rene Reede <rene.reede@gmail.com>
2024-01-11 22:05:15 +0200

    Add: List loop
    This loop allows certain guests in the building and not others.

--- 01_Html/RockPaperScissors/Loops and Arrays/javascript.js
+++ 01_Html/RockPaperScissors/Loops and Arrays/javascript.js
@@ -64,8 +64,23 @@ function reverseString(string) {
 
 reverseString('Rene Reede');
 
+// Add or refuse people in the list
 
-function removeFromArray() {
+const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
 
-    
+const admitted = document.querySelector('.admitted');
+const refused = document.querySelector('.refused');
+
+admitted.textContent = 'Admit: ';
+refused.textContent = 'Refuse: ';
+
+for (const person of people) {
+  if (person === 'Phil' || person === 'Lola') {
+    refused.textContent += `${person}, `;
+  } else {
+    admitted.textContent += `${person}, `;
+  }
 }
+
+refused.textContent = refused.textContent.slice(0,refused.textContent.length-2) + '.';
+admitted.textContent = admitted.textContent.slice(0,admitted.textContent.length-2) + '.';
